b78d48f72797a42f45f824685683fa9f0638a3fd
andiraco <34205178+andiraco@users.noreply.github.com>
2022-06-02 19:37:25 -0600

    First commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
+import React from 'react';
+
+const Footer = ({length}) => {
+    const today = new Date();
+  return (
+      <footer>
+          <p>{length} List {length === 1? "item" :"item"}</p>
+          <p>Copyright &copy; {today.getFullYear()}</p>
+      
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/LineItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+
+const LineItem = ({item, handleCheck,handleDelete}) => {
+  return (
+     <li className = "item">
+            <input
+              type="checkbox"
+              onChange={()=>handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label
+              style={(item.checked) ? {textDecoration: 'line-through'}: null}
+              onDoubleClick={() => handleCheck(item.id)}
+              checked={item.checked}
+            >{item.item}</label>
+            <FaTrashAlt
+              onClick={()=>handleDelete(item.id)}
+              role="button"
+              tabIndex="0"
+              arial-label={`Delete ${item.item}`}
+            />
+          </li>
+  );
+}
+
+export default LineItem;
